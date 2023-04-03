1557104fe27263c8f3c924a51d250eb8483666af
Aayush Sharma <aayushsharma47163@gmail.com>
2023-04-04 00:01:54 +0530

    Files modified successfully

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('Hello world');
+
+window.onscroll = function() {changeColor()};
+
+// function changeColor() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementById("container").style.backgroundColor = "#00c1b5";
+//     console.log("scrolled");
+//   }
+//   else if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//     document.getElementById("container").style.backgroundColor = "#ff651a";
+//   }
+//   else {
+//     document.getElementById("container").style.backgroundColor = "white";
+//   }
+// }
+
